69756718b2e615c8856a31d9fc97bf8d438e2b0e
Jonbi2 <marcellobardus8@gmail.com>
2018-12-09 08:42:18 +0100

    endpoint touched

--- src/controllers/documents/create/create.document.controller.ts
+++ src/controllers/documents/create/create.document.controller.ts
@@ -1,6 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post, Body, Headers } from "@nestjs/common";
+import { CreateDocumentDto } from "./create.document.dto";
 
 @Controller("api/v1/documents/create")
 export class CreateDocumentsController {
   constructor() {}
+  @Post()
+  async create(
+    @Headers() headers: any,
+    @Body() registerUserDto: CreateDocumentDto
+  ) {}
 }
